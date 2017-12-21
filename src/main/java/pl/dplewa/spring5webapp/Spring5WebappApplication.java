ef6541c359b812f88d9c27c9d8c34bfe7f54ac6e
Dominik Plewa <plewad@gmail.com>
2017-12-21 17:22:49 +0100

    skeleton app from spring-initializr.io

---
+++ src/main/java/pl/dplewa/spring5webapp/Spring5WebappApplication.java
@@ -0,0 +1,12 @@
+package pl.dplewa.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+}
